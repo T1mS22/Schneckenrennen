8974ab4731e3862a5b3aecc124d67d8f94d676ef
tischnei <tischnei@ARLT3711-17.ads.uni-magdeburg.de>
2018-09-25 15:42:52 +0200

    Wettbuero v3

--- src/wettbuero/Wettbuero.java
+++ src/wettbuero/Wettbuero.java
@@ -21,6 +21,37 @@ public class Wettbuero {
 		Wette NameSchnecke = new Wette(schneckenName, wettEinsatz, spieler);
 		Wetten.add(NameSchnecke);
 		
+		
 	}
+	
+	public int getRennenNr() {
+		return RennenNr;
+	}
+
+	public void setRennenNr(int rennenNr) {
+		RennenNr = rennenNr;
+	}
+	
+	public void rennenDruchfuehren(int RennenNr) {
+		setRennenNr(RennenNr);
+		
+	}
+	
+	public void Ausgabe() {
+		
+		System.out.println("Gewonnen hat: " + siegerSchnecke );
+		System.out.println("Es geb " + Wetten.size() + " Wetten.");
+		for(int i = 0; i <= Wetten.size(); i++) {
+			if(Wetten.get(i).getNameSchnecke() == siegerSchnecke) {
+				float gewinn = Wetten.get(i).getEinsatz() * FAKTOR_WETTE;
+				String gewinner = Wetten.get(i).getNameSpieler();
+				System.out.println("Gewonnen hat: " + gewinner);
+				System.out.println("Er hat: " + gewinn + "€ gewonnen.");
+			}
+		System.out.println("Das Rennen ist beendet. Danke für's mitspielen.");	
+		}
+	}
+
+	
 	
 }
