ecd63a9e23650b18b7cd835c4b172ae7bfd758af
sfluegel <sfluegel@ARLT3711-14.ads.uni-magdeburg.de>
2018-09-25 15:09:17 +0200

    Rennen

---
+++ src/schneckenrennen/Rennen.java
@@ -0,0 +1,57 @@
+package schneckenrennen;
+
+public class Rennen {
+	private String rennenName = "";
+	private int anzahlSchnecken = 0;
+	private ArrayList<Rennschnecke> teilnehmendeSchnecken = new ArrayList<Rennschnecke>();
+	private int laengeStrecke = 0;
+	
+	public Rennen(String rennenName, int laengeStrecke) {
+		this.rennenName = rennenName;
+		this.laengeStrecke = laengeStrecke;
+	}
+	
+	public void addRennschnecke(Rennschnecke neueSchnecke) {
+		this.teilnehmendeSchnecken.add(neueSchnecke);
+		this.anzahlSchnecken += 1;
+	}
+	
+	public void removeRennschnecke(String name) {
+		/*for (Rennschnecke item: teilnehmendeSchnecken) {
+			if (item.equals(name)) {
+				this.teilnehmendeSchnecken.remove(item);
+				this.anzahlSchnecken -= 1;
+			}
+		}*/
+		if (this.teilnehmendeSchnecken.contains(name) == true) {
+			this.teilnehmendeSchnecken.remove(name);
+			this.anzahlSchnecken -= 1;
+		}
+	}
+	
+	public String toString() {
+		String ausgabe = this.rennenName + ", " + this.anzahlSchnecken + " Schnecken, "
+				+ this.laengeStrecke + "lang. Teilnehmer: " 
+				+ this.teilnehmendeSchnecken.toString();
+		return ausgabe;
+	}
+	
+	public Rennschnecke ermittleGewinner() {
+		Rennschnecke siegerSchnecke = null;
+		for (Rennschnecke item: teilnehmendeSchnecken) {
+			if (item.getDistance() == this.laengeStrecke) {
+				siegerSchnecke = item;
+			}
+		}
+		return siegerSchnecke;
+	}
+	
+	private void lassSchneckeKriechen() {
+		for (Rennschnecke item: teilnehmendeSchnecken) {
+			item.kriechen();
+		}
+	}
+	public void durchfuehren() {
+		
+	}
+}
